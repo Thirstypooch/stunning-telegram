42dac471507652eeb8d6fac51c3776ba0eb171d7
Thirstypooch <thirstypooch@protonmail.com>
2020-10-19 14:25:42 -0500

    connected mongo db

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const { ApolloServer } = require('apollo-server')
 const gql = require('graphql-tag')
 const mongoose = require('mongoose')
 
+const { MONGO_DB } = require('./config')
+
 const typeDefs = gql`
   type Query {
     sayHi: String!
@@ -19,8 +21,13 @@ const server = new ApolloServer({
   resolvers,
 })
 
-mongoose.connect()
-
-server.listen({port: 5000}).then(res => {
-  console.log(`🤖 at ${res.url}`);
-})
+mongoose
+  .connect(MONGO_DB, { useNewUrlParser: true, useUnifiedTopology: true  })
+  .then(() => {
+    console.log('Mongo DB 🍾')
+    return server.listen({ port: 5000 })
+    
+  })
+  .then((res) => {
+    console.log(`🤖 at ${res.url}`)
+  })
