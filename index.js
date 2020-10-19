1347938633dc3a7271ecbba1c2ce8ef76d28f153
Thirstypooch <thirstypooch@protonmail.com>
2020-10-19 15:06:27 -0500

    split models, types & resolvers

--- index.js
+++ index.js
@@ -1,20 +1,10 @@
 const { ApolloServer } = require('apollo-server')
-const gql = require('graphql-tag')
 const mongoose = require('mongoose')
 
+const typeDefs = require('./graphql/TypeDefs')
+const resolvers = require('./graphql/resolvers')
+const Post = require('./models/Post')
 const { MONGO_DB } = require('./config')
-
-const typeDefs = gql`
-  type Query {
-    sayHi: String!
-  }
-`
-
-const resolvers = {
-  Query: {
-    sayHi: () => 'Thirsty Pooch',
-  },
-}
 
 const server = new ApolloServer({
   typeDefs,
@@ -22,11 +12,10 @@ const server = new ApolloServer({
 })
 
 mongoose
-  .connect(MONGO_DB, { useNewUrlParser: true, useUnifiedTopology: true  })
+  .connect(MONGO_DB, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Mongo DB 🍾')
     return server.listen({ port: 5000 })
-    
   })
   .then((res) => {
     console.log(`🤖 at ${res.url}`)
